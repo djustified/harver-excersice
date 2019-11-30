890a1724680be18c2e913d50dea6b128c0d92b69
Mohamed Irshad <irshad@ideabits.se>
2019-11-30 10:46:50 +0530

    task3.2: write fizzbuzz program created in task2 using async function

--- src/index.js
+++ src/index.js
@@ -6,10 +6,25 @@ console.log('It works!');
 const startNumber = 1;
 const endNumber = 100;
 
+getWordToPrint = (number, wordToPrint) => {
+  const isMultipleOfThree = number % 3 === 0;
+  const isMultipleOfFive = number % 5 === 0;
+
+  if (isMultipleOfThree && isMultipleOfFive) {
+    wordToPrint = 'FizzBuzz';
+  } else if (isMultipleOfThree) {
+    wordToPrint = 'Fizz';
+  } else if (isMultipleOfFive) {
+    wordToPrint = 'Buzz';
+  }
+  return wordToPrint;
+};
+
 async function printRandomNumbers() {
   for (i = startNumber; i <= endNumber; i++) {
     const randomWord = await getRandomWord();
-    const formattedString = `${i}: ${randomWord}`;
+    const wordToPrint = getWordToPrint(i, randomWord);
+    const formattedString = `${i}: ${wordToPrint}`;
     console.log(formattedString);
   }
 }
