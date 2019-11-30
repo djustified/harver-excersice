d4f17ec5ce0bd80aa6f92b0eb934d1b6dc088f0e
Mohamed Irshad <irshad@ideabits.se>
2019-11-30 12:45:27 +0530

    task5-task2 store generated strings of task2 in a file in the project root

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ console.log('It works!');
 // YOUR CODE HERE
 
 // Set Path to store the generated strings
-const path = '../randomWords-task1-async.txt'
+const path = '../randomWords-task2-async.txt'
 
-//Remove file if it already exists
+// Remove file if it already exists
 fileSystem.unlink(path, (error) => {
   if (error) {
     return
@@ -18,15 +18,32 @@ fileSystem.unlink(path, (error) => {
 const startNumber = 1;
 const endNumber = 100;
 
+getWordToPrint = (number, wordToPrint) => {
+  const isMultipleOfThree = number % 3 === 0;
+  const isMultipleOfFive = number % 5 === 0;
+
+  if (isMultipleOfThree && isMultipleOfFive) {
+    wordToPrint = 'FizzBuzz';
+  } else if (isMultipleOfThree) {
+    wordToPrint = 'Fizz';
+  } else if (isMultipleOfFive) {
+    wordToPrint = 'Buzz';
+  } else if (!wordToPrint) {
+    wordToPrint = `It shouldn't break anything!`;
+  }
+  return wordToPrint;
+};
+
 async function printRandomNumbers() {
   for (i = startNumber; i <= endNumber; i++) {
     let randomWord;
     try {
       randomWord = await getRandomWord({ withErrors: true });
     } catch (error) {
-      randomWord = `It shouldn't break anything!`;
+      randomWord = false;
     }
-    const formattedString = `${i}: ${randomWord}`;
+    const wordToPrint = getWordToPrint(i, randomWord);
+    const formattedString = `${i}: ${wordToPrint}`;
     fileSystem.appendFile(
       path,
       `${formattedString}\n`,
