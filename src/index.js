0c22f212052b44a26423e8d8964dfc863b4a0413
Mohamed Irshad <irshad@ideabits.se>
2019-11-30 12:38:30 +0530

    task5-task1 store generated strings in a file in the project root

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const { getRandomWord } = require('word-maker');
+const fileSystem = require('fs');
 
 console.log('It works!');
 
 // YOUR CODE HERE
+
+// Set Path to store the generated strings
+const path = '../randomWords-task1-async.txt'
+
+//Remove file if it already exists
+fileSystem.unlink(path, (error) => {
+  if (error) {
+    return
+  }
+})
+
 const startNumber = 1;
 const endNumber = 100;
 
@@ -15,7 +27,15 @@ async function printRandomNumbers() {
       randomWord = `It shouldn't break anything!`;
     }
     const formattedString = `${i}: ${randomWord}`;
-    console.log(formattedString);
+    fileSystem.appendFile(
+      path,
+      `${formattedString}\n`,
+      (error) => {
+        if (error) throw error;
+        if (i === 100) {
+          console.log(`Saved as ${path}. You can find this file  in the project root folder`)
+        }
+      });
   }
 }
 
