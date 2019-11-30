f132e2fc30f6e8582868914b15108736cb6d16b4
Mohamed Irshad <irshad@ideabits.se>
2019-11-30 12:00:05 +0530

    task4-task2 add error handling to task2 synchronous solution

--- src/index.js
+++ src/index.js
@@ -6,13 +6,31 @@ console.log('It works!');
 const startNumber = 1;
 const endNumber = 100;
 
+getWordToPrint = number => {
+  let wordToPrint;
+  try {
+    wordToPrint = getRandomWordSync({ withErrors: true });
+  } catch (error) {
+    wordToPrint = false;
+  }
+
+  const isMultipleOfThree = number % 3 === 0;
+  const isMultipleOfFive = number % 5 === 0;
+
+  if (isMultipleOfThree && isMultipleOfFive) {
+    wordToPrint = 'FizzBuzz';
+  } else if (isMultipleOfThree) {
+    wordToPrint = 'Fizz';
+  } else if (isMultipleOfFive) {
+    wordToPrint = 'Buzz';
+  } else if (!wordToPrint) {
+    wordToPrint = `It shouldn't break anything!`;
+  }
+  return wordToPrint;
+};
+
 for (i = startNumber; i <= endNumber; i++) {
-  let randomWord;
-    try {
-      randomWord =  getRandomWordSync({ withErrors: true });
-    } catch (error) {
-      randomWord = `It shouldn't break anything!`;
-    }
+  const randomWord = getWordToPrint(i);
   const formattedString = `${i}: ${randomWord}`;
   console.log(formattedString);
 }
