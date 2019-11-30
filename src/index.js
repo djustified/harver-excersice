73ef26c09df7c524c24cb0eb647ffe81a21e7cef
Mohamed Irshad <irshad@ideabits.se>
2019-11-30 09:59:16 +0530

    task1: print 1-100 to the console with a random word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const { getRandomWordSync, getRandomWord } = require('word-maker');
+const { getRandomWordSync } = require('word-maker');
 
 console.log('It works!');
 
 // YOUR CODE HERE
+const startNumber = 1;
+const endNumber = 100;
+
+for (i = startNumber; i <= endNumber; i++) {
+  const randomWord = getRandomWordSync();
+  const formattedString = `${i}: ${randomWord}`;
+  console.log(formattedString);
+}
